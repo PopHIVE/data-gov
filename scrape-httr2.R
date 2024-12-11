2f7d05468db953b50c5c2697abd5dd38b9998c73
howardbaik <howardhowiebaek@gmail.com>
2024-12-10 21:41:28 -0500

    WIP code for scraping Data.gov

--- scrape-httr2.R
+++ scrape-httr2.R
@@ -6,15 +6,25 @@ base_url <- "https://catalog.data.gov/api/3/action/package_search"
 
 # Search term = "firearm"
 req <- request(base_url) |> 
-  req_url_query(q="firearm")
+  # Query "firearm" with maximum 1000 rows to return
+  req_url_query(q = "firearm", rows = 1000)
 
 # Perform request and fetch response from server
 resp <- req |> req_perform()
 # Parse JSON
 resp_json <- resp |> resp_body_json()
 
-# Grab results
+# Grab results of HTTP request
 results <- resp_json[[3]]$results
 
-# Sixth result (NCHS - VSRR Quarterly provisional estimates for selected indicators of mortality)
-results[[6]]
+# Grab URL for downloading CSV and download
+for (result_index in length(results)) {
+  resources <- results[[result_index]]$resources
+
+  for (resources_index in length(resources)) {
+      if (resources[[resources_index]]$format == "CSV") {
+        download_url <- resources[[resources_index]]$url
+        download.file(download_url, "TODO_name_of_dataset.csv")
+      }  
+  }
+}  
