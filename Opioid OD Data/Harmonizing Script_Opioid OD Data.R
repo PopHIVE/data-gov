3e9736cf659d955a44f2c3ab80104bbe2f77bc96
Shelby Golden <shelby.golden@yale.edu>
2025-01-06 10:37:02 -0500

    Add opioid and rsv raw data.

---
+++ Opioid OD Data/Harmonizing Script_Opioid OD Data.R
@@ -0,0 +1,21 @@
+## ----------------------------------------------------------------
+## Harmonizing Opioid OD Data
+##
+## Date: December 23rd, 2024
+## Source:
+##    - CDC SUDORS and DOSE
+##    - Agency for Healthcare Research and Quality (AHRQ)
+
+## ----------------------------------------------------------------
+## SET UP THE ENVIRONMENT
+
+
+
+
+## ----------------------------------------------------------------
+## LOAD THE DATA
+
+
+# https://wonder.cdc.gov/controller/saved/D77/D419F696
+
+
---
+++ RSV Infections Data/Harmonizing Script_RSV Infections Data.R
@@ -0,0 +1,19 @@
+## ----------------------------------------------------------------
+## Harmonizing RSV Infections Data
+##
+## Date: December 23rd, 2024
+## Source:
+##    - RSV-NET from the CDC (connected with NREVSS data)
+
+## ----------------------------------------------------------------
+## SET UP THE ENVIRONMENT
+
+
+
+
+## ----------------------------------------------------------------
+## LOAD THE DATA
+
+
+
+
