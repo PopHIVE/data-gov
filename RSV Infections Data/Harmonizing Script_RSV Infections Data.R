b637e3208904edd1de5efb851f837dee9cbae9a7
Shelby Golden <shelby.golden@yale.edu>
2025-01-23 14:54:20 -0500

    Added RSV raw datasets (sans Google Trends).

--- RSV Infections Data/Harmonizing Script_RSV Infections Data.R
+++ RSV Infections Data/Harmonizing Script_RSV Infections Data.R
@@ -8,11 +8,31 @@
 ## ----------------------------------------------------------------
 ## SET UP THE ENVIRONMENT
 
+library("readxl")
+library("readr")
+library("tidyr")
+library("dplyr")
+library("stringr")
+
+"%!in%" <- function(x,y)!("%in%"(x,y))
+
 
 
 
 ## ----------------------------------------------------------------
 ## LOAD THE DATA
+
+rsv_net <- read_csv("RSV Infections Data/Raw Download/RSV-NET All Seasons_Interactive Dashboard Output_Downloaded 12.23.20224.csv") %>%
+  as.data.frame()
+
+
+# bronchiolitis (infants only), rsv
+
+# EPIC, NERVESS, and Google Trends not expected to have stratification except by state.
+
+
+
+
 
 
 
