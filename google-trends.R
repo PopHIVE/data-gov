1998f049b537d28af3de54aabce9c4a0ed91c211
howardbaik <howardbaik43@gmail.com>
2025-01-30 15:36:20 -0500

    Delete google-trends.R

--- google-trends.R
+++
@@ -1,8 +0,0 @@
-# Use development version, instead of CRAN version
-# devtools::install_github("PMassicotte/gtrendsR")
-library(gtrendsR)
-
-state_abb_us <- paste0('US-', state.abb)
-df <- gtrends(keyword = "rsv", geo = state_abb_us[1],
-              time = "2020-01-01 2025-01-21",
-              category = 0, gprop='web')
