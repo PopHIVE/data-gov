179f6d71f668d9345468f8611976a2972ee5a63a
Shelby Golden <shelby.golden@yale.edu>
2025-01-30 22:37:08 -0500

    Fix map option. Add one RSV static plot.

--- Suggested Static Plots.R
+++ Suggested Static Plots.R
@@ -11,8 +11,14 @@ library("tidyr")
 library("dplyr")
 library("scales")
 library("ggplot2")
+library("lubridate")
 library("usmap")
+library("zoo")
+library("mapview")
 library("cowplot")
+library("tigris")
+library("leaflet")
+library("sf")
 
 "%!in%" <- function(x,y)!("%in%"(x,y))
 
@@ -24,6 +30,10 @@ library("cowplot")
 
 opioid_od <- read_csv("Opioid OD Data/Harmonized Opioid Overdose Datasets_01.23.2025.csv") %>%
   as.data.frame()
+
+rsv <- read_csv("RSV Infections Data/Harmonized RSV Infections Datasets_01.30.2025.csv") %>%
+  as.data.frame()
+
 
 
 
@@ -178,6 +188,142 @@ title <- ggdraw() +
 
 # Display the plot with title.
 plot_grid(title, plot_together, ncol = 1, rel_heights = c(1, 1))
+
+
+
+
+# -----------------------------
+# US Map plot. Interactive.
+
+
+us_geo <- tigris::states(cb = TRUE, resolution = '20m') %>%
+  shift_geometry()
+
+geo_data_wonder <- opioid_od %>%
+  # Filter the placeholder numerical values.
+  filter(Count %!in% 7777 & Count %!in% 8888 & Count %!in% 9999) %>%
+  
+  # Switch between the two settings, and change the subtitle name.
+  filter(Setting %in% "Medical Facility - Inpatient") %>%
+  #filter(Setting %in% "Medical Facility - Outpatient or ER") %>%
+  
+  # Filter the metadata settings.
+  filter(Dataset %in% "CDC WONDER", State %in% datasets::state.name, 
+         Year %in% 2022, Quarter %in% NA,
+         `Underlying Cause of Death` %in% "All", Drug %in% "All Opioids",
+         Characteristic %in% "Not Stratified", Level %in% "N/A") %>%
+  rename(NAME = State) %>%
+  left_join(., us_geo, by = "NAME")
+
+
+# Counts in AHRQ dataset.
+geo_data_ahrq <- opioid_od %>%
+  # Filter the placeholder numerical values.
+  filter(Count %!in% 7777 & Count %!in% 8888 & Count %!in% 9999) %>%
+  
+  # Switch between the two settings, and change the subtitle name.
+  filter(Setting %in% "Medical Facility - Inpatient") %>%
+  #filter(Setting %in% "Medical Facility - Outpatient or ER") %>%
+  
+  # Filter the metadata settings.
+  filter(Dataset %in% "AHRQ", State %in% datasets::state.name, 
+         Year %in% 2022, Quarter %in% NA,
+         `Underlying Cause of Death` %in% "All", Drug %in% "All Opioids",
+         Characteristic %in% "Not Stratified", Level %in% "N/A") %>%
+  rename(NAME = State) %>%
+  left_join(., us_geo, by = "NAME")
+
+
+# Different maps.
+# https://leaflet-extras.github.io/leaflet-providers/preview/
+
+map_wonder <- mapview(st_as_sf(geo_data_wonder), zcol = "Count", layer.name = "CDC WONDER", map.types = c("TomTom.Basic", "OpenAIP"))
+map_ahrq <- mapview(st_as_sf(geo_data_ahrq), zcol = "Count", layer.name = "AHRQ", map.types = c("TomTom.Basic", "OpenAIP"))
+
+
+map_wonder|map_ahrq
+
+
+
+
+## ----------------------------------------------------------------
+## RSV INFECTIONS DATA
+
+
+# -----------------------------
+# Basic time-series line plot.
+
+
+# Define the start of the infection season.
+season_start = week("01-10-2020")
+
+rsv %>%
+  # Drop down to choose the HHS region: Region 1 through 10.
+  filter(Region %in% "Region 10") %>% 
+  
+  
+  # Filter the metadata settings.
+  filter(`Region Type` %in% "HHS", `Week Observed` %!in% NA, 
+         Dataset %in% "NREVSS Lab") %>%
+  # Shift the week number so that week = 0 at the beginning of the start of the
+  # infection season.
+  mutate(week = ifelse(week(`Week Observed`) - season_start < 0, 
+                       week(`Week Observed`) - season_start + 53, 
+                       week(`Week Observed`) - season_start) ) %>%
+  
+  # Plot settings and features.
+  ggplot(data = ., aes(x = week, y = `Crude Rate`)) +
+  geom_line(aes(color = Season)) +
+  facet_grid(cols = vars(`Diagnostic Test Type`)) +
+  labs(title = "Respiratory Syncytial Virus Infection (RSV) Rate",
+       subtitle = "Surveillance Program: NREVSS Lab.",
+       x = "Weeks Since October", y = "Crude Rate (per 100,000)") +
+  theme_minimal()
+
+
+
+
+
+# -----------------------------
+# US Map plot.
+
+# Code to find mathes between AHRQ and CDC WONDER
+#opioid_od[opioid_od$Dataset %in% "AHRQ", "Setting"] %>% unique()
+
+# Generate a side-by-side set of plots showing "Drug = All Opioids" counts
+# by state in 2022. Toggle the setting to show either inpatient or ER.
+
+
+# Counts in AHRQ dataset.
+ahrq_map_plot <- opioid_od %>%
+  # Filter the placeholder numerical values.
+  filter(Count %!in% 7777 & Count %!in% 8888 & Count %!in% 9999) %>%
+  
+  # Switch between the two settings, and change the subtitle name.
+  filter(Setting %in% "Medical Facility - Inpatient") %>%
+  #filter(Setting %in% "Medical Facility - Outpatient or ER") %>%
+  
+  # Filter the metadata settings.
+  filter(Dataset %in% "AHRQ", State %in% datasets::state.name, 
+         Year %in% 2022, Quarter %in% NA,
+         `Underlying Cause of Death` %in% "All", Drug %in% "All Opioids",
+         Characteristic %in% "Not Stratified", Level %in% "N/A") %>%
+  
+  # plot_usmap() requires specific nomenclature for the column with states.
+  rename(state = State) %>%
+  
+  # Plot settings and features.
+  plot_usmap(data = ., values = "Count", color = "red") + 
+  scale_fill_continuous(
+    low = "white", high = "red", name = "Count (2022)", label = scales::comma
+  ) + 
+  labs(title = "AHRQ") +
+  theme(legend.position = "")
+
+
+
+
+
 
 
 
